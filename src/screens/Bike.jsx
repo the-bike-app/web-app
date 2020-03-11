f0f8fe149c31daab0495dece20de32773819acd7
Christopher Starke <CJstarke@Christophers-MacBook-Air.local>
2020-03-11 10:01:16 -0400

    edited bike.js

--- src/screens/Bike.jsx
+++ src/screens/Bike.jsx
@@ -68,7 +68,7 @@ class Bike extends Component {
                 )
               }
             >
-              shmeEdit
+              Edit
                         </button>
           </div>
         </div>
