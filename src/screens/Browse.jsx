34be713710adcf958d75041e2581a110511cd81b
Devon McCalmon <devon.mccalmon@yahoo.com>
2020-03-16 13:28:42 -0400

    working on search button

--- src/screens/Browse.jsx
+++ src/screens/Browse.jsx
@@ -1,42 +1,49 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Footer from '../components/shared/Footer'
-import { Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
-function Browse(props) {
-  const showBikes = () => {
-    if (props.bikes) {
-      return props.bikes.map(bike => {
-        return (
-          <Link to = {`/bikes/${bike._id}`}>
-            <div className="item" key={bike._id}>
-              <h2>{bike.brand}</h2>
-              <p>{bike.type}</p>
-              <p>${bike.price}</p>
-              <p>{bike.location}</p>
-              <img src={bike.image} alt={bike.type} />
-            </div>
-          </Link>
-        )
-      })
-    } else {
-      return <p>No bikes</p>
-    }
+class Browse extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
   }
 
-  return (
+  showBikes = () => {
 
-    <>
-      <div className="browse-title">
-        <h1>Browse</h1>
-        <div className='bikes-container'>
-          {showBikes()}
+    const { bikes } = this.props
+
+    return bikes.map(bike => {
+      
+      return (
+        <Link to={`/bikes/${bike._id}`}>
+          <div className="item" key={bike._id}>
+            <h2>{bike.brand}</h2>
+            <p>{bike.type}</p>
+            <p>${bike.price}</p>
+            <p>{bike.location}</p>
+            <img src={bike.image} alt={bike.type} />
+          </div>
+        </Link>
+      )
+    })
+  }
+
+  render() {
+    return (
+
+      <>
+        <div className="browse-title">
+          <h1>Browse</h1>
+          <div className='bikes-container'>
+            {this.showBikes()}
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
-    </>
+      </>
 
 
-  )
+    )
+  }
 }
 
 export default Browse
